60aeb644470a9e0cfb25bd11cce68545ab431212
EddieCurio <edcarril@ucsd.edu>
2017-12-05 18:02:36 -0800

    Finished Milestone 4 "View a List of Chat Messages"

---
+++ ParseLab/ChatTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ChatTableViewCell.swift
+//  ParseLab
+//
+//  Created by my mac on 12/5/17.
+//  Copyright © 2017 ecproductions. All rights reserved.
+//
+
+import UIKit
+
+class ChatTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var messageTextLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+}
--- ParseLab/ChatViewController.swift
+++ ParseLab/ChatViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 import Parse
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+
+
+    
+    
+    
+    var messages: [PFObject] = []
     
     
     @IBOutlet weak var chatMessageTextField: UITextField!
     
+    @IBOutlet weak var tableView: UITableView!
     @IBAction func onSendTapped(_ sender: Any) {
         
         let chatMessage = PFObject(className: "Message")
@@ -36,11 +45,59 @@ class ChatViewController: UIViewController {
     }
     
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //Not very safe code.
+        let messageCell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath) as! ChatTableViewCell
+        
+        
+        let message = self.messages[indexPath.row]
+        
+        let messageText = message["text"] as? String
+        
+        messageCell.textLabel?.text = messageText
+        
+        
+        return messageCell
+        
+    }
+    
+    func onTimer(){
+      // Add code to be run periodically
+        let query = PFQuery(className: "Message")
+        
+        query.findObjectsInBackground { (messageObjects: [PFObject]?, error :Error?) in
+            if let error = error {
+                 print("There was trouble receving PFObjects")
+            }else if let messages = messageObjects {
+            
+                self.messages = messages
+                self.tableView.reloadData()
+            }
+        }
+    
+    }
+    
+    func initTimer(){
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer:Timer) in
+            self.onTimer()
+        }
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        initTimer()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
